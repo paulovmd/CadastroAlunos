0d4103aab0f7db9183370e498f4dca043dd7bc41
Paulo Vinícius Moreira Dutra <paulo.dutra@ifsudestemg.edu.br>
2025-06-12 10:46:02 -0300

    Update

--- CadastroAlunos/Program.cs
+++ CadastroAlunos/Program.cs
@@ -3,14 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Ativa o uso dos controllers e vincula as views(páginas) com os
+//controllers responsáveis pela requisições.
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+
 //Cria a conexão com o banco de dados. Neste caso, utilizando
 //o banco de dados SQLite.
 builder.Services.AddDbContext<CadastroAlunosDBContext>(
     options => options.UseSqlite("Data source=cadastroalunos.db" ) );
 
-
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseRouting();
+
+//Definindo qual será a página principal  a ser chamada.
+app.MapControllerRoute(name: "default",
+    pattern : "{controller=Home}/{action=Index}");
 
 app.Run();
